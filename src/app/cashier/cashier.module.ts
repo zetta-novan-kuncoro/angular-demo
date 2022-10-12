c8955b450269863759b8f1470cd0805f6c16893c
Novan R. Kuncoro <novan.kuncoro@zettacamp.pro>
2022-10-12 15:33:20 +0700

    Add a parent component for the cashier module

--- src/app/cashier/cashier.module.ts
+++ src/app/cashier/cashier.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CashierRoutingModule } from './cashier-routing.module';
 import { MenuComponent } from './menu/menu.component';
 import { PaymentComponent } from './payment/payment.component';
+import { CashierComponent } from './cashier/cashier.component';
 
 
 @NgModule({
   declarations: [
     MenuComponent,
-    PaymentComponent
+    PaymentComponent,
+    CashierComponent
   ],
   imports: [
     CommonModule,
