a1fdef5e4ce230e181d4c4a310d4a7292c3d5a80
Novan R. Kuncoro <novan.kuncoro@zettacamp.pro>
2022-10-12 15:28:39 +0700

    Add cashier module

---
+++ src/app/cashier/cashier.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CashierRoutingModule } from './cashier-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CashierRoutingModule
+  ]
+})
+export class CashierModule { }
