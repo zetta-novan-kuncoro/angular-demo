3569ac62316c2ca58aa1164b01d30f35e139fadb
Novan R. Kuncoro <novan.kuncoro@zettacamp.pro>
2022-10-12 15:30:22 +0700

    Add employee availability component in the employee module

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { AvailabilityComponent } from './availability/availability.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AvailabilityComponent
+  ],
   imports: [
     CommonModule,
     EmployeeRoutingModule
