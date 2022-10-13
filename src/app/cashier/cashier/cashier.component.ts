253abbaddf91b4c2657309b19364cef4a8feb69e
Novan R. Kuncoro <novan.kuncoro@zettacamp.pro>
2022-10-13 09:47:32 +0700

    Add menu item list functionality

--- src/app/cashier/cashier/cashier.component.ts
+++ src/app/cashier/cashier/cashier.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cashier.component.scss']
 })
 export class CashierComponent implements OnInit {
+  public items: Item[] = [
+    { id: '1', name: 'Cafe Latte', price: 5000 },
+    { id: '2', name: 'Vanilla Latte', price: 5000 },
+    { id: '3', name: 'Caramel Macchiato', price: 5000 },
+    { id: '4', name: 'Americano', price: 5000 },
+    { id: '5', name: 'Affogato', price: 5000 },
+    { id: '6', name: 'Matcha Latte', price: 5000 },
+    { id: '7', name: 'Cappuccino', price: 5000 },
+    { id: '8', name: 'Mochaccino', price: 5000 },
+    { id: '9', name: 'Japanese Iced Coffee', price: 5000 },
+    { id: '10', name: 'Vietnam Drip', price: 5000 },
+  ]
 
   constructor() { }
 
